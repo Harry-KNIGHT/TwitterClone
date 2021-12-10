783aaeada31f2b2fbf5bcf9922a9a0e088387d3a
Harry-KNIGHT <elliotharryk@gmail.com>
2021-12-10 21:58:21 +0100

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Elliot knight on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
